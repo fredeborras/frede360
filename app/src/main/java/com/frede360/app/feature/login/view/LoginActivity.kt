7c90793b2022852d9b1a0ecce4fe2bf9e5342a7f
Frede Borras <frede.borras@gmail.com>
2020-06-18 18:54:56 +0200

    Frede360: Creating Project

---
+++ app/src/main/java/com/frede360/app/feature/login/view/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.frede360.app.feature.login.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.frede360.app.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
