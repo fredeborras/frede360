0a80fd197c1e238bb5be6bea671afe4c77e28b9a
Frede Borras <frede.borras@gmail.com>
2020-06-18 19:28:37 +0200

    Feature: Login - Adding InputLayout for email and pwd

--- app/src/main/java/com/frede360/app/feature/login/view/LoginActivity.kt
+++ app/src/main/java/com/frede360/app/feature/login/view/LoginActivity.kt
@@ -1,10 +1,10 @@
 package com.frede360.app.feature.login.view
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.frede360.app.R
+import com.frede360.app.commons.view.Frede360BaseActivity
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : Frede360BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
